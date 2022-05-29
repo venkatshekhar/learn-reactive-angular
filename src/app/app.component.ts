ed60e758dd066e95d6383bc379858cf5546c86da
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-29 21:16:28 +0530

    handling login action and showing login and logout option in menu based on login action

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { LoadingService } from './loading/loading.service';
 import { MessagesService } from './messages/messages.service';
+import { AuthStore } from './services/auth.store';
 
 
 
@@ -11,7 +12,7 @@ import { MessagesService } from './messages/messages.service';
 })
 export class AppComponent implements OnInit {
 
-  constructor() {
+  constructor(public auth: AuthStore) {
 
   }
 
@@ -20,7 +21,9 @@ export class AppComponent implements OnInit {
 
   }
 
+
   logout() {
+    this.auth.logout();
 
   }
 

47b009da0d035a2f2d2ceb9768d737b70bc78342
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-29 16:13:05 +0530

    reactive approach to loading on and off in loading service method

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -33,18 +33,17 @@ export class HomeComponent implements OnInit {
 
   reloadCourses() {
 
-    this.loadingService.loadingOn();
-
     const courses$ = this.courseService.loadAllCourses()
       .pipe(
-        map(courses => courses.sort(sortCoursesBySeqNo)),
-        finalize(() => this.loadingService.loadingOff())
+        map(courses => courses.sort(sortCoursesBySeqNo))
       );
 
-    this.beginnerCourses$ = courses$
+    const loadCourses$ = this.loadingService.showLoaderUntilCompleted(courses$);
+
+    this.beginnerCourses$ = loadCourses$
       .pipe(map(courses => courses.filter(course => course.category == "BEGINNER")));
 
-    this.advancedCourses$ = courses$
+    this.advancedCourses$ = loadCourses$
       .pipe(map(courses => courses.filter(course => course.category == "ADVANCED")));
 
   }
--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
-import { BehaviorSubject, Observable } from "rxjs";
+import { BehaviorSubject, Observable, of } from "rxjs";
+import { concatMap, finalize, tap } from "rxjs/operators";
 
 // Shared service
 @Injectable()
@@ -10,8 +11,13 @@ export class LoadingService{
     loading$: Observable<boolean> = this.loadingSubject.asObservable();
 
     showLoaderUntilCompleted<T>(obs$ : Observable<T>): Observable<T>{
-        return undefined;
-    }
+        return of(null)
+            .pipe(
+                tap(()=> this.loadingOn()),    // trigger sideeffect to start loader before return observable
+                concatMap(()=> obs$),   // return the received obs 
+                finalize(()=> this.loadingOff())  // trigger when above method completes or errors out
+            )
+    } 
 
     loadingOn(){
         this.loadingSubject.next(true);

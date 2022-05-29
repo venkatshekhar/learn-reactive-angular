7a5d14fa6fd4727ca6ac4ce1719fe3a05a46faec
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-29 10:35:12 +0530

    sharing http response for multiple http request to avoid multiple calls

--- src/app/services/courses.service.ts
+++ src/app/services/courses.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { Observable } from "rxjs";
-import { map } from "rxjs/operators";
+import { map, shareReplay } from "rxjs/operators";
 import { Course } from "../model/course";
 
 
@@ -13,7 +13,11 @@ export class CourseService {
     constructor(private http: HttpClient) { }
 
     loadAllCourses(): Observable<Course[]> {
-        return this.http.get<Course[]>('/api/courses').pipe(map(res => res['payload']));
+        return this.http.get<Course[]>('/api/courses')
+            .pipe(
+                map(res => res['payload']),
+                shareReplay()
+            );
     }
 
 }

4a345cbe0f3089af5a3d3de07c7eabc29b1c1ba9
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-05-29 17:52:08 +0530

    showing errors message in message panel

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
 import {Message} from '../model/message';
 import {tap} from 'rxjs/operators';
+import { MessagesService } from './messages.service';
 
 @Component({
   selector: 'messages',
@@ -12,13 +13,16 @@ export class MessagesComponent implements OnInit {
 
   showMessages = false;
 
-  constructor() {
+  errors$: Observable<string[]>;
 
+  constructor(public messagesService: MessagesService) {
+    console.log('Created Messages component');
   }
 
   ngOnInit() {
-
-
+    this.errors$ = this.messagesService.errors$.pipe(
+      tap(()=> this.showMessages= true)
+    );
   }
 
 
--- src/app/messages/messages.service.ts
+++ src/app/messages/messages.service.ts
@@ -1,10 +1,17 @@
 import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable, Subject } from "rxjs";
+import { filter } from "rxjs/operators";
 
 @Injectable()
-export class MessagesService{
+export class MessagesService {
 
-        showErrors(...errors: string[]){
-            
+        private subject = new BehaviorSubject<string[]>([]);
+
+        errors$: Observable<string[]> = this.subject.asObservable()
+                .pipe(filter(messages => messages && messages.length > 0));
+
+        showErrors(...errors: string[]) {
+                this.subject.next(errors);
         }
 
 }

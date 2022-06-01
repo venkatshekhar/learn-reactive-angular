75e6b1c7518e74b84ba2616cb335babfa2876ade
venkatshekhar <shekhar.shetty1992@gmail.com>
2022-06-01 07:42:37 +0530

    adding onpush changeDetection

--- src/app/lesson/lesson.component.ts
+++ src/app/lesson/lesson.component.ts
@@ -1,10 +1,11 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input, OnInit } from '@angular/core';
 import { Lesson } from '../model/lesson';
 
 @Component({
   selector: 'lesson',
   templateUrl: './lesson.component.html',
-  styleUrls: ['./lesson.component.css']
+  styleUrls: ['./lesson.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class LessonComponent implements OnInit  {
 
